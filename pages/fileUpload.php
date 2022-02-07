4831ce9143a161ce51f241845fa60c58f08218a6
shaown <shaownakter.1995@gmail.com>
2022-02-07 13:27:04 +0600

    day_16_hasib

--- pages/fileUpload.php
+++ pages/fileUpload.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <div class="card card-body">
-                    <form action="action.php" method="POST">
+                    <form action="action.php" method="POST" enctype="multipart/form-data">
                         <div class="form-group row">
                             <label for="" class="col-md-4 col-form-label">Image</label>
                             <div class="col-md-8">
